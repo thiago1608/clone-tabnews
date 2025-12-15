6676e253a223b238a88b19094deef9ef183b1258
thiago1608 <134345689+thiago1608@users.noreply.github.com>
2025-12-15 17:50:04 +0000

    adiciona script  lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Curso Filipe Deschamps - Implementação do https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home () {
-    return <h1>Bom dia meus amores que Deus abençoe nossa familia, Amo VCS</h1>
+function Home() {
+  return <h1>Bom dia meus amores que Deus abençoe nossa familia, Amo VCS</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
+}
 export default Home;
