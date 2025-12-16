c3f6bcfe9614c8a924637f83ae7229e1053fdaef
thiago1608 <134345689+thiago1608@users.noreply.github.com>
2025-12-16 14:12:35 +0000

    install  e configurado watch

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Bom dia meus amores que Deus abençoe nossa familia, Amo VCS</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste2");
-}
 export default Home;
