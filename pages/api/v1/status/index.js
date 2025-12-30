1de0b8e5a86731725b3c40c288d9f7e00f81d923
thiago1608 <134345689+thiago1608@users.noreply.github.com>
2025-12-30 14:15:00 +0000

    finish endepoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,32 @@
-function status(request, response) {
-  response.status(200).json({ chave: "Alunos do curso.dev" });
+import database from "infra/database.js";
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query("Show max_connections;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int from pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsResult,
+      },
+    },
+  });
 }
 export default status;
--- tests/integration/api/v1/status/index.js
+++
@@ -1,8 +0,0 @@
-import database from "infra/database.js";
-
-async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
-}
-export default status;
