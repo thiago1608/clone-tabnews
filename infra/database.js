9cab4c4b5b3779ccedf48403d9679a12b798f1bb
thiago1608 <134345689+thiago1608@users.noreply.github.com>
2026-01-06 13:28:37 +0000

    handles database ssl connections

--- infra/database.js
+++ infra/database.js
@@ -2,14 +2,15 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGRES_LOCALHOST,
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? true : false,
   });
   console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_LOCALHOST,
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
